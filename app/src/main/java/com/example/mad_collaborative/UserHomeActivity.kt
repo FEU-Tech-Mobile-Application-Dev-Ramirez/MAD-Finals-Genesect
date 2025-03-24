9b68c0c4677dbc9cc4369dfb543690fa221dc297
MADP002 <mariusdelposo@gmail.com>
2025-03-24 19:58:42 +0800

    refactor(User home): Implemented binding instead of using findviewbyid

--- app/src/main/java/com/example/mad_collaborative/UserHomeActivity.kt
+++ app/src/main/java/com/example/mad_collaborative/UserHomeActivity.kt
@@ -1,35 +1,35 @@
 package com.example.mad_collaborative
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import com.example.mad_collaborative.databinding.UserHomeBinding
 
 class UserHomeActivity : ComponentActivity() {
-    @SuppressLint("MissingInflatedId")
+
+    private lateinit var binding: UserHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.user_home)
+        binding = UserHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
 
-        val btnTournaments = findViewById<Button>(R.id.btnTournaments)
-        val btnNews = findViewById<Button>(R.id.btnNews)
-        val tournamentT1 = findViewById<View>(R.id.T1)
-        val newsSection = findViewById<View>(R.id.newsSection)
 
-        tournamentT1.visibility = View.VISIBLE
-        newsSection.visibility = View.GONE
+        binding.T1.visibility = View.VISIBLE
+        binding.newsSection.visibility = View.GONE
 
-        btnTournaments.setOnClickListener {
-            if (newsSection.visibility == View.VISIBLE) {
-                newsSection.visibility = View.GONE
-                tournamentT1.visibility = View.VISIBLE
+        binding.btnTournaments.setOnClickListener {
+            if (binding.newsSection.visibility == View.VISIBLE) {
+                binding.newsSection.visibility = View.GONE
+                binding.T1.visibility = View.VISIBLE
             }
         }
 
-        btnNews.setOnClickListener {
-            tournamentT1.visibility = View.GONE
-            newsSection.visibility = View.VISIBLE
+        binding.btnNews.setOnClickListener {
+            binding.T1.visibility = View.GONE
+            binding.newsSection.visibility = View.VISIBLE
         }
     }
 }
